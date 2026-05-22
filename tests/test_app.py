d0deec1489ab6a00238adcb1cdb6660684ad2b61
manoel cândido <candidomanoel78@gmail.com>
2026-05-22 13:20:09 -0300

    test: cria novos testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -43,15 +43,15 @@ def test_update_user(client):
     response = client.put(
         '/users/1',
         json={
-            'username': 'candido',
-            'email': 'candido@example.com',
+            'username': 'bob',
+            'email': 'bob@example.com',
             'password': 'mynewpassword',
         },
     )
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'username': 'candido',
-        'email': 'candido@example.com',
+        'username': 'bob',
+        'email': 'bob@example.com',
         'id': 1,
     }
 
@@ -65,6 +65,24 @@ def test_update_user_should_return_not_found(client):
             'password': 'mynewpassword',
         },
     )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_get_user(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_get_user_should_return_not_found(client):
+    response = client.get('/users/291')
+
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not found'}
 
