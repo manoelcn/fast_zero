a86ee70e09d71848503300dd618dedd220a10dd9
manoel cândido <candidomanoel78@gmail.com>
2026-05-27 10:26:25 -0300

    test: corrige teste get_user para buscar usuário do banco

--- tests/test_app.py
+++ tests/test_app.py
@@ -120,22 +120,22 @@ def test_update_integrity_error(client, user):
     }
 
 
-# def test_get_user(client):
-#     response = client.get('/users/1')
+def test_get_user(client, user):
+    response = client.get(f'/users/{user.id}')
 
-#     assert response.status_code == HTTPStatus.OK
-#     assert response.json() == {
-#         'username': 'bob',
-#         'email': 'bob@example.com',
-#         'id': 1,
-#     }
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': user.username,
+        'email': user.email,
+        'id': user.id,
+    }
 
 
-# def test_get_user_should_return_not_found(client):
-#     response = client.get('/users/291')
+def test_get_user_should_return_not_found(client):
+    response = client.get('/users/291')
 
-#     assert response.status_code == HTTPStatus.NOT_FOUND
-#     assert response.json() == {'detail': 'User not found'}
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
 
 
 def test_delete_user(client, user):
