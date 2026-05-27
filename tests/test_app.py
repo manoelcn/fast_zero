ae45ee6e86a9148c44ee37709efa8fe84449d7bb
manoel cândido <candidomanoel78@gmail.com>
2026-05-27 11:19:51 -0300

    test: teste de criação de token

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,6 +10,18 @@ def test_root_deve_retornar_ok_e_ola_mundo(client):
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert 'access_token' in token
+    assert 'token_type' in token
+
+
 def test_create_user(client):
     response = client.post(
         '/users/',
