ead99e4324a070f8b723cbcb5929ee823df85488
manoel cândido <candidomanoel78@gmail.com>
2026-05-27 11:36:04 -0300

    feat: altera update_user para usar current_user

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -10,6 +10,7 @@ from fast_zero.models import User
 from fast_zero.schemas import Message, Token, UserList, UserPublic, UserSchema
 from fast_zero.security import (
     create_access_token,
+    get_current_user,
     get_password_hash,
     verify_password,
 )
@@ -104,22 +105,24 @@ def read_user(user_id: int, session: Session = Depends(get_session)):
 
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(
-    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+    user_id: int,
+    user: UserSchema,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
 ):
-
-    db_user = session.scalar(select(User).where(User.id == user_id))
-    if not db_user:
+    if current_user.id != user_id:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+            status_code=HTTPStatus.FORBIDDEN, detail='Not enough permission'
         )
-    try:
-        db_user.username = user.username
-        db_user.password = get_password_hash(user.password)
-        db_user.email = user.email
-        session.commit()
-        session.refresh(db_user)
 
-        return db_user
+    try:
+        current_user.username = user.username
+        current_user.password = get_password_hash(user.password)
+        current_user.email = user.email
+        session.commit()
+        session.refresh(current_user)
+
+        return current_user
 
     except IntegrityError:
         raise HTTPException(
