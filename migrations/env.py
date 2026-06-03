e9e56f725bf1ec098cc9d189cc9c7ad2afc1e312
manoel cândido <candidomanoel78@gmail.com>
2026-06-03 11:48:39 -0300

    feat: faz com que migrações sejam executadas de forma síncrona

--- migrations/env.py
+++ migrations/env.py
@@ -1,6 +1,9 @@
+import asyncio
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
+from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
@@ -74,6 +77,31 @@ def run_migrations_online() -> None:
 
         with context.begin_transaction():
             context.run_migrations()
+
+
+def do_run_migrations(connection): 
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations(): 
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+def run_migrations_online(): 
+    asyncio.run(run_async_migrations())
+
 
 
 if context.is_offline_mode():
